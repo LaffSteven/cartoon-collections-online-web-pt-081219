8c96b0bccbea3b043fb0b1ebe423e462402092a0
Steven Laff <steve.laff@gmail.com>
2019-08-21 20:16:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
     if cheese_types.include?(type)
-      
+      cheese
     end
   end
 end
